290ee47521d021f4a78e7d1d1679e14027f54dce
daniel-oguejiofor <d.oguejiofor@maontech.com>
2026-06-22 16:41:28 +0100

    Use CloudFront for API routing

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,13 +1,14 @@
-export const environment = {
-  production: false,
-  authUrl: 'http://localhost:8000/auth-service/api',
-  employeeUrl: 'http://localhost:8000/employee-service/api',
-  wsUrl: 'ws://localhost:8083', // Add WebSocket URL
-};
-
 // export const environment = {
 //   production: false,
-//   authUrl: 'http://api-gateway:8000/auth-service/api',
-//   employeeUrl: 'http://api-gateway:8000/employee-service/api',
-//   wsUrl: 'ws://api-gateway:8083', // Add WebSocket URL
+//   authUrl: 'http://localhost:8000/auth-service/api',
+//   employeeUrl: 'http://localhost:8000/employee-service/api',
+//   wsUrl: 'ws://localhost:8083', // Add WebSocket URL
 // };
+
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  authUrl: 'http://ems-alb-1263798516.eu-west-1.elb.amazonaws.com/auth-service/api',
+  employeeUrl: 'http://ems-alb-1263798516.eu-west-1.elb.amazonaws.com/employee-service/api',
+  wsUrl: 'ws://localhost:8083'
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  authUrl: 'http://ems-alb-1263798516.eu-west-1.elb.amazonaws.com/auth-service/api',
-  employeeUrl: 'http://ems-alb-1263798516.eu-west-1.elb.amazonaws.com/employee-service/api',
+  authUrl: 'https://d2wam7n2862k0g.cloudfront.net/auth-service/api',
+  employeeUrl: 'https://d2wam7n2862k0g.cloudfront.net/employee-service/api',
   wsUrl: 'ws://localhost:8083'
 };
